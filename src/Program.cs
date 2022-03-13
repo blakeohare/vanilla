f9ec14d4884d569ca6185e60017a3c5c987628f9
Blake O'Hare <mspaintmaestro@gmail.com>
2022-03-12 18:07:35 -0800

    You can now create a file in the repo root called DEBUG_ARGS.txt, the last line of which will be used as the command line arguments when you run debug builds from the repo directly.

--- src/Program.cs
+++ src/Program.cs
@@ -6,10 +6,39 @@ namespace Vanilla
     {
         static void Main(string[] args)
         {
-            // TODO: arg checking and DEBUG_ARGS file support.
-            string sourceDir = args[0];
+            args = GetEffectiveArgs() ?? args;
 
-            new Parser().Parse(sourceDir);
+            string sourceEntryFile = args[0];
+
+            new Parser().Parse(sourceEntryFile);
+        }
+
+        private static string[] GetEffectiveArgs()
+        {
+#if DEBUG
+            string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+            string binDebug = System.IO.Path.Combine("src", "bin", "Debug");
+            if (currentDirectory.Contains(binDebug))
+            {
+                string srcRoot = currentDirectory.Split(binDebug)[0];
+                string debugArgsFile = System.IO.Path.Combine(srcRoot, "DEBUG_ARGS.txt");
+                if (System.IO.File.Exists(debugArgsFile))
+                {
+                    string text = System.IO.File.ReadAllText(debugArgsFile);
+                    string[] lines = text.Trim().Split('\n');
+                    string lastLine = lines[lines.Length - 1].Trim();
+                    if (lastLine.Length > 0)
+                    {
+                        string[] debugArgs = lastLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                        if (debugArgs.Length > 0)
+                        {
+                            return debugArgs;
+                        }
+                    }
+                }
+            }
+#endif
+            return null;
         }
     }
 }
